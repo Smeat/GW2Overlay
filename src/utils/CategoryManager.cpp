73a5e2078ccc6fa568d7d4a92a1199295af7ebd6
Kevin <smeatsan@gmail.com>
2020-08-19 20:47:49 +0200

    Convert names to lower case These are keys after all...

--- src/utils/CategoryManager.cpp
+++ src/utils/CategoryManager.cpp
@@ -1,4 +1,6 @@
 #include "CategoryManager.h"
+#include <algorithm>
+#include <cctype>
 
 #include "POI.h"
 #include "xml/pugixml.hpp"
@@ -34,6 +36,9 @@ void CategoryManager::load_taco_xml(const std::string& filename) {
 			cat->m_height_offset = node.attribute("heightOffset").as_float(0);
 			cat->m_display_name = node.attribute("DisplayName").value();
 			cat->m_name = node.attribute("name").value();
+			std::transform(cat->m_name.begin(), cat->m_name.end(),
+						   cat->m_name.begin(),
+						   [](unsigned char c) { return std::tolower(c); });
 			if (parent) {
 				auto ret = parent->m_children.insert(cat);
 				parent = *std::get<0>(ret);
@@ -71,7 +76,7 @@ void CategoryManager::load_taco_xmls(
 	const std::vector<std::string>& filenames) {
 	for (auto iter = filenames.begin(); iter != filenames.end(); ++iter) {
 		this->load_taco_xml(*iter);
-		print_categories(&this->m_categories);
+		//		print_categories(&this->m_categories);
 	}
 }
 
